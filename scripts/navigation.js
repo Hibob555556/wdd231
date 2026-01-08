6b7270c9de536e6770c501dcec8df3d31cc94186
Cayden Lunt <cayden.lunt@gmail.com>
2026-01-08 01:45:49 -0700

    finished: week01 learning activities

--- scripts/navigation.js
+++ scripts/navigation.js
@@ -1,7 +1,11 @@
 // Store interactive elements for reference
 const navbutton = document.querySelector('#ham-button');
 
+// store container elements for reference 
+const navBar = document.querySelector('#nav-bar');
+
 // toggle show class
 navbutton.addEventListener('click', () => {
     navbutton.classList.toggle("show");
+    navBar.classList.toggle('show');
 });
