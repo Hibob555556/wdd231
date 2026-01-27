f557a9878e69a07ba36e63c8ed5f1fe1156694ab
Cayden Lunt <cayden.lunt@gmail.com>
2026-01-26 23:55:49 -0700

    updating week03 assignment

--- week-03/scripts/get-weather.js
+++ week-03/scripts/get-weather.js
@@ -1,41 +1,56 @@
 const BASE_URL = "https://api.openweathermap.org/data/2.5/weather?";
-const LAT = 6.63;
-const LON = 49.74;
+const LON = 6.63;
+const LAT = 49.74;
 const UNITS = "imperial";
 
-// NOTE:
-// This API key is intentionally exposed because this project
-// is deployed as a static GitHub Pages site for a class assignment.
-// In production, API requests should be proxied through a backend
-// where secrets are stored securely.
+// NOTE: 
+// This API key is intentionally exposed because this project 
+// is deployed as a static GitHub Pages site for a class assignment. 
+// In production, API requests should be proxied through a backend 
+// where secrets are stored securely. 
 const API_KEY = "e6955d20253e5ae3f87c89bfa2650b17";
-// BAD BAD BAD BAD BAD PRACTICE
 
 const PARAMS = ["lat", "lon", "units", "APPID"];
 const PARAM_VALUES = [LAT, LON, UNITS, API_KEY];
-
-let URL = BASE_URL;
+let url = BASE_URL;
 let i = 0;
 PARAMS.forEach(param => {
     if (i < PARAM_VALUES.length - 1)
-        URL += `${param}=${PARAM_VALUES[i]}&`
+        url += `${param}=${PARAM_VALUES[i]}&`
     else
-        URL += `${param}=${PARAM_VALUES[i]}`
+        url += `${param}=${PARAM_VALUES[i]}`
     i++;
 });
+apiFetch(url);
 
 async function apiFetch(url) {
     try {
         let response = await fetch(url);
         if (!response.ok)
-            response.text = "Failed to fetch endpoint";
-        else {
-            let data = response.json();
-            console.log(data)
-        }
+            throw new Error("Failed to fetch endpoint");
+        let data = await response.json();
+        displayResults(data);
     } catch (err) {
         console.error(`Error fetching endpoint "${url}. Error:"`, err);
+        return 0;
     }
 }
 
-apiFetch();
+function displayResults(weatherData) {
+    // elements 
+    const CURRENT_TEMP_OUT = document.querySelector('#current-temp');
+    const WEATHER_ICON_OUT = document.querySelector('#weather-icon');
+    const WEATHER_CAPTION = document.querySelector('#weather-conds');
+
+    // data 
+    const ICON_ID = weatherData['weather'][0]['icon'];
+    const TEMP = weatherData['main']['temp'];
+    const ICON_URL = `https://openweathermap.org/img/wn/${ICON_ID}@2x.png`;
+    const WEATHER_DESC = weatherData['weather'][0]['description']
+
+    // display 
+    CURRENT_TEMP_OUT.textContent = `${TEMP} °F`;
+    WEATHER_ICON_OUT.setAttribute('src', ICON_URL);
+    WEATHER_ICON_OUT.setAttribute('alt', WEATHER_DESC);
+    WEATHER_CAPTION.textContent = WEATHER_DESC;
+}
